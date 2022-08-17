a0d021b5f0eea3aa7d12b25b4fe708cab18d741c
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-08-17 09:51:05 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -15,7 +15,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.markdown(' 1 - Choose your operating parameters and date for the synthesis <br> 2 - Click on <b>Generate protocol</b> first than on <b>Download protocol</b>', unsafe_allow_html=True)
 with col2:
-    st.markdown('3 - Upload the python file dowloaded on the Opentrons App <br> 4 - Download and fill Excel file for the preparation of reagents and labwares',
+    st.markdown('3 - Upload the python file dowloaded on the [Opentrons App] ("https://opentrons.com/ot-app/") <br> 4 - Download and fill Excel file for the preparation of reagents and labwares',
         unsafe_allow_html=True)
 
 def convert(min):
