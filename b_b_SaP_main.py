ddfc447b2bde37a3642a5d8b72633c21c73817ce
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-08-22 13:37:12 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -231,7 +231,7 @@ volume_W2 = 0.050 * number_of_cycle+0.1
 
 color_enzyme = 'background-color: green'
 
-table_volume = np.full((8,12), 0)
+table_volume = np.full((8,12), '')
 for i in range(4):
     table_volume[i,0] = 'A = '+str(volume_nucleotide*1000)+' µL'
     table_volume[i,1] = round(volume_enzyme,3)*1000
@@ -240,7 +240,7 @@ for i in range(4):
     table_volume[i,4] = round(volume_W2,3)*1000
 
 def color_reageants(cell):
-    if cell != 0:
+    if cell is not '':
         return 'background-color: #c4b6d9'
 np.round(table_volume, 3)        
 df = pd.DataFrame(
