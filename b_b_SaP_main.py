26e3c57a6967fa3065d7d90c11d628daab4018ca
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-08-30 15:21:42 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -35,7 +35,7 @@ image_reservoir = Image.open("images/USA_reservoir.PNG")
 image_filter_pall = Image.open("images/Pall_on_mani.PNG")
 image_filter_grenier = Image.open("images/Grenier.PNG")
 
-col1, col2, col3, col4 = st.columns([1, 1, 1, 1.5], gap="medium")
+col1, col2, col3, col4 = st.columns([2, 1, 1, 1.5], gap="medium")
 
 with col1:
     number_of_cycle = st.slider('Barcode length', min_value = 1, max_value = 19, value = 13)
