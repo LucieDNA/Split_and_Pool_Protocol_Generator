1da61b821eeee39b73c6db126627392b11b9a46d
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-02 11:12:58 +0200

    Update g_SaP_add_end.py

--- g_SaP_add_end.py
+++ g_SaP_add_end.py
@@ -1,11 +1,12 @@
 from e_SaP_elementaryCommands import *
 
 
+end_seq = 'ATCGAATCGA'
+
 def add_end_seq(protocolFile, multi_pipet, single_pipet, labware_list, MARC_COMPORT, pool_well, end_seq):
     if end_seq is not '':
         for base in end_seq:
             add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_COMPORT, pool_well, base)
-
 
 def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_COMPORT, pool_well, base):
 
@@ -42,15 +43,28 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     if base == 'T':
         base_well = 3
         base_tip = 63
+    if base == 'U':
+        base_well = 4
+        base_tip = 47
+    if base == 'X':
+        base_well = 3
+        base_tip = 46
+
 
     #Elongation start
-
-    comment_WL(protocolFile, "Addition of "+ base)
-    ## Nucleotide
+    startVac(protocolFile, MARC_COMPORT)
     pickup_tips_single_WL(protocolFile, pipet300_single, tips_300, base_tip)
+    delay_WL(protocolFile, seconds=15, minutes=0)
+    startVent(protocolFile, MARC_COMPORT)
+    delay_WL(protocolFile, 6, 0)
+    stopVac(protocolFile, MARC_COMPORT)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
     aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, base_well, 25, 1.5)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
+    stopVent(protocolFile, MARC_COMPORT)
+
+    comment_WL(protocolFile, "Addition of "+ base)
+    ## Nucleotide
     dispense_SaP_WL(protocolFile, pipet300_single, FilterPlate, pool_well, 25 + 2 * AIR_GAP_VOL, DISP_FLOW_RATE)
     blow_out_WL(protocolFile, pipet300_single)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
@@ -58,7 +72,7 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     ## Enzyme
     pickup_tips_single_WL(protocolFile, pipet300_single, tips_300, 68)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
-    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 8, 25, 1.5)
+    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 12, 25, 1.5)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
     dispense_SaP_WL(protocolFile, pipet300_single, FilterPlate, pool_well, 25 + 2 * AIR_GAP_VOL, DISP_FLOW_RATE)
     blow_out_WL(protocolFile, pipet300_single)
@@ -77,7 +91,7 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     delay_WL(protocolFile, 6, 0)
     stopVac(protocolFile, MARC_COMPORT)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
-    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 16, 50, ASP_FLOW_RATE)
+    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 20, 50, ASP_FLOW_RATE)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
     stopVent(protocolFile, MARC_COMPORT)
 
@@ -110,7 +124,7 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     # Stir, wait and vacuum
     startStirring(protocolFile, MARC_COMPORT)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
-    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 28, 50, ASP_FLOW_RATE)
+    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 29, 50, ASP_FLOW_RATE)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
     delay_WL(protocolFile, seconds=25, minutes=0)
     stopStirring(protocolFile, MARC_COMPORT)
@@ -135,7 +149,7 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     delay_WL(protocolFile, 6, 0)
     stopVac(protocolFile, MARC_COMPORT)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
-    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 32, 50, ASP_FLOW_RATE)
+    aspirate_SaP_WL(protocolFile, pipet300_single, ReagentReservoir, 36, 50, ASP_FLOW_RATE)
     air_gap_WL(protocolFile, pipet300_single, AIR_GAP_VOL)
     stopVent(protocolFile, MARC_COMPORT)
 
@@ -149,5 +163,3 @@ def add_single_nuc(protocolFile, multi_pipet, single_pipet, labware_list, MARC_C
     return_WL(protocolFile, pipet300_single)
     delay_WL(protocolFile, seconds=10, minutes=0)
     stopStirring(protocolFile, MARC_COMPORT)
-
-    vacuum(protocolFile, MARC_COMPORT, 25)
