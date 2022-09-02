553efa4e2b82315dbac9f8ed3d8cfe1c1f081c2a
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-02 11:44:40 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -63,7 +63,7 @@ with col1:
 with col2:
     end_seq = st.text_input('Addition of sequence at the end of the barcodes', 'TCGTCGGCAGCGUCAGAUGUGUAUAAGAGACAG')
     
-duration = convert(19.24 * number_of_cycle+10*(len(start_seq)+len(end_seq)))
+duration = convert(19.4 * number_of_cycle+10*(len(start_seq)+len(end_seq)))
 
 with infos:
     st.markdown("Number of cycles &emsp; **" + str(number_of_cycle) + "**", unsafe_allow_html=True)
