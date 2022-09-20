52daa5ace460b363a03a93710bb661a3fa5bbe92
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-20 13:43:33 +0200

    Update c_SaP_genProtocol.py

--- c_SaP_genProtocol.py
+++ c_SaP_genProtocol.py
@@ -7,8 +7,12 @@ import datetime
 from g_SaP_add_end import *
 from g_b_SaP_end_seq_double import *
 
-AIR_GAP_VOL = 10
 
+AIR_GAP_VOL = 10
+DISPENSE_HEIGHT = str(-1)
+ASPIRATION_DEPTH = labware_list[3][0]
+ASP_FLOW_RATE = labware_list[3][1]
+DISP_FLOW_RATE = labware_list[3][2]
 
 def genProtocol(general_parameters, labware_list_and_loc, pipetting_condition, precision_pipetting_param, synthesis_date, start_seq, end_seq, single_psp, double_psp, well_psp, control_synth):
 
