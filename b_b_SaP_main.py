62dee6cc09aa762f7a662309f7819b3c3ddf7c9c
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-27 16:42:38 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -313,6 +313,6 @@ df = pd.DataFrame(
     index=('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'),
     columns=(str(i) for i in range(1,13)))
 
-tableau_1 = st.tabs(['Reagents plate'])
+tableau_1, tableau_2 = st.tabs(['Reagents plate', 'Tip rack'])
 with tableau_1:
     table = st.dataframe(df.style.applymap(color_reageants), width=1500)
