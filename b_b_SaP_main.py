85041506205bcb67a1545696887f18589aabcdae
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-27 16:50:52 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -320,7 +320,7 @@ with tableau_1:
     
 table_tip = np.full((8,12), '                  ')
 for i in range(number_of_cycle+1):
-    table_tip[i%8, i//8] = "Cycle " + i
+    table_tip[i%8, i//8] = "Cycle " + str(i)
 
 df = pd.DataFrame(
     table_tip,
