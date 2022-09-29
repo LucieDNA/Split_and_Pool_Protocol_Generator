2d7bfbde7a4501443205e39114df82a5a5708fe7
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-29 09:50:50 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -570,7 +570,7 @@ with tab1:
     with col2:
         st.markdown('**Buffer**')
         st.markdown("<span style='background-color: #e3beca;'>Elongation buffer = " +str(volume_enzyme+volume_E_2)+" µL</span>", unsafe_allow_html=True)
-        st.markdown('Wash 1 = ' +str(volume_W1+volume_W1_2)+" µL")
-        st.markdown('Deblock = ' +str(volume_D+volume_D_2*2)+" µL")
-        st.markdown('Wash 2 = ' +str(volume_W2+volume_W2_2)+" µL")
+        st.markdown("<span style='background-color: #f1d77f;'>Wash 1 = " +str(volume_W1+volume_W1_2)+" µL</span>", unsafe_allow_html=True)
+        st.markdown("<span style='background-color: #f1d77f;'Deblock = " +str(volume_D+volume_D_2*2)+" µL</span>", unsafe_allow_html=True)
+        st.markdown("<span style='background-color: #f1d77f;'>Wash 2 = " +str(volume_W2+volume_W2_2)+" µL</span>", unsafe_allow_html=True)
     st.text('Number of tips = '+ str(nombre_tip))
