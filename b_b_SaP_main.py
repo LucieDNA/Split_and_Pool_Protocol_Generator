33adbef71433fcac5196326a3a208cd2382923b5
LucieDNA <108665354+LucieDNA@users.noreply.github.com>
2022-09-29 11:33:24 +0200

    Update b_b_SaP_main.py

--- b_b_SaP_main.py
+++ b_b_SaP_main.py
@@ -594,4 +594,7 @@ with tab1:
         st.markdown("<span style='background-color: #b3c5da;'>Wash 2 for transfer = " +str(volume_Ps_split_1*3+volume_Ps_split_2*3+volume_Ps_split_3*3+volume_Ps_pool*3*4+270*nb_end_well)+" µL</span>", unsafe_allow_html=True)
     with col3:
         st.markdown('**Buffer with enzymes preparation**')
+        st.markdown("<span style='background-color: #e3beca;'>Elongation buffer = " +str(volume_enzyme+volume_E_2*nb_end_well)+" µL</span>", unsafe_allow_html=True)
+        st.markdown("<span style='background-color: #e3beca;'>Enzyme M96 = " +str((volume_enzyme+volume_E_2*nb_end_well)*1/5)+" µL</span>", unsafe_allow_html=True)
+        st.markdown("<span style='background-color: #e3beca;'>Enzyme buffer = " +str((volume_enzyme+volume_E_2*nb_end_well)*4/5)+" µL</span>", unsafe_allow_html=True)
     st.text('Number of tips = '+ str(nombre_tip))
